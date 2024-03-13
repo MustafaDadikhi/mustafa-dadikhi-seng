80c55d9744ea00efff5415e1ea54207385fc6c2b
MustafaDadikhi <103628356+MustafaDadikhi@users.noreply.github.com>
2024-03-13 20:07:57 +0300

    Initial commit

---
+++ src/content_option.js
@@ -0,0 +1,96 @@
+const logotext = "JOHN";
+const meta = {
+    title: "Mustafa Dadikhi",
+    description: "I’m Mustafa Dadikhi Software engineering student at hasan kalyoncu university",
+};
+
+const introdata = {
+    title: "I’m Mustafa Dadikhi",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I develop mobile apps",
+    },
+    description: "I’m Mustafa Dadikhi Software engineering student at hasan kalyoncu university",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "about me",
+    aboutme: "As a dedicated software engineering student at Hasan Kalyoncu University, I am driven by a passion for innovation and problem-solving in the realm of technology. With a keen interest in developing efficient and scalable software solutions, I am continuously expanding my skills and knowledge to meet the ever-evolving demands of the industry. Through hands-on experience and academic rigor, I strive to contribute meaningfully to the advancement of software engineering while embracing challenges with creativity and determination.",
+};
+const worktimeline = [{
+        jobtitle: "Student At Software Eng. Department",
+        where: "HKU",
+        date: "2024",
+    },
+
+];
+
+const skills = [{
+        name: "Python",
+        value: 90,
+    },
+    {
+        name: "HTML",
+        value: 85,
+    },
+    {
+        name: "CSS",
+        value: 80,
+    },
+    {
+        name: "Javascript",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Expert At UI & UX Design",
+    },
+    {
+        title: "Mobile Apps",
+        description: "solutions for mobile",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Wordpress&SEO services",
+    },
+];
+
+const dataportfolio = [];
+
+const contactConfig = {
+    YOUR_EMAIL: "mustafa.dadikhi@gmail.com",
+    YOUR_FONE: "(539)376-6229",
+    description: "Call Me ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
